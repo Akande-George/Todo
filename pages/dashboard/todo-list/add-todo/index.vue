16d0a036f16c673775ad238bc752c6036f3d066b
George Akande <george.akande.goa@gmail.com>
2020-06-23 11:13:57 +0100

    changes on index.vue

--- pages/dashboard/todo-list/add-todo/index.vue
+++ pages/dashboard/todo-list/add-todo/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div id="todo-list">
+  <div id="add-todo">
       <DashboardNav />
+      <div id="add-todo-container">
+          <h4>ADD TODO</h4>
+          <section>
+              <form>
+                  
+              </form>
+          </section>
+      </div>
   </div>
 </template>
 
